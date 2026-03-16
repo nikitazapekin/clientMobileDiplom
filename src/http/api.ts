d455a97a91298d10b8ae0b75eaa025a551aabcc7
Nikita <nikitazapekin>
2026-03-16 16:16:26 +0300

    fix(comments): Update comments

--- src/http/api.ts
+++ src/http/api.ts
@@ -3,7 +3,7 @@ import type { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axio
 import axios from 'axios';
 
 // Cloudflare Tunnel к бэкенду (localhost:3002)
-export const BASE_URL = 'https://scheme-petersburg-jaguar-literary.trycloudflare.com';
+export const BASE_URL = 'https://blend-bacon-bargain-searched.trycloudflare.com';
 const $api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // Важно для отправки cookies
